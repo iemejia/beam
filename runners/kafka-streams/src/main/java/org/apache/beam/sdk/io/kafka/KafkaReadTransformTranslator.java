f757fbdd86baed56b01c28598b0d0d90dd952db4
Kyle Winkelman <kyle.winkelman@optum.com>
2019-01-04 09:16:19 -0600

    Spotless

--- runners/kafka-streams/src/main/java/org/apache/beam/sdk/io/kafka/KafkaReadTransformTranslator.java
+++ runners/kafka-streams/src/main/java/org/apache/beam/sdk/io/kafka/KafkaReadTransformTranslator.java
@@ -1,7 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.kafka;
 
 import java.util.List;
-
 import org.apache.beam.sdk.io.UnboundedSource;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
@@ -26,7 +42,8 @@ public class KafkaReadTransformTranslator {
     }
   }
 
-  public static <K, V> KStream<K, V> kafkaTopicRead(StreamsBuilder streamsBuilder, KafkaUnboundedSource<K, V> kafkaUnboundedSource) {
+  public static <K, V> KStream<K, V> kafkaTopicRead(
+      StreamsBuilder streamsBuilder, KafkaUnboundedSource<K, V> kafkaUnboundedSource) {
     KafkaIO.Read<K, V> spec = kafkaUnboundedSource.getSpec();
     List<String> topics = spec.getTopics();
     Serde<K> keySerde = serdeFrom(spec.getKeyDeserializer());
