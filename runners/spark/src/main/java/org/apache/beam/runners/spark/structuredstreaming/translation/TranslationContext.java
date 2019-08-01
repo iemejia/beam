2fb9d6821d1d52144986d324eeb1567aeac0683c
Ismaël Mejía <iemejia@gmail.com>
2019-08-01 09:32:50 +0200

    fixup! Fix hardcoded value

--- runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
@@ -80,10 +80,14 @@ public class TranslationContext {
 
     // By default, Spark defines 200 as a number of sql partitions. This seems too much for local
     // mode, so try to align with value of "sparkMaster" option in this case.
+    // We should not overwrite this value (or any user-defined spark configuration value) if the
+    // user has already configured it.
     String sparkMaster = options.getSparkMaster();
-    if (sparkMaster != null && sparkMaster.startsWith("local[")) {
+    if (sparkMaster != null
+        && sparkMaster.startsWith("local[")
+        && System.getProperty("spark.sql.shuffle.partitions") == null) {
       int numPartitions =
-          Integer.valueOf(sparkMaster.substring("local[".length(), sparkMaster.length() - 1));
+          Integer.parseInt(sparkMaster.substring("local[".length(), sparkMaster.length() - 1));
       if (numPartitions > 0) {
         sparkConf.set("spark.sql.shuffle.partitions", String.valueOf(numPartitions));
       }
